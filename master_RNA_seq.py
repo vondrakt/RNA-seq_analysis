c2975275ccc47f70c2534ef504aa25ee19580367
Tihana Vondrak <51750652+vondrakt@users.noreply.github.com>
2023-01-11 12:57:53 +0100

    Add files via upload

--- master_RNA_seq.py
+++ master_RNA_seq.py
@@ -29,54 +29,54 @@ if options.path is None or options.samples is None or options.replicates is None
 samples = options.samples.split(',')
 replicates = options.replicates.split(',')
 
-# for replicate in replicates:
-#     name1 = options.path+'/'+samples[0]+replicate
-#     name2 = options.path+'/'+samples[1]+replicate
-#     print(name1, name2)
-#     # Trimming the reads
-#     trimming_command = 'trimmomatic PE -threads 30 -phred33 %s.fastq.gz' \
-#                        ' %s.fastq.gz %s_1P.fastq.gz %s_1U.fastq.gz %s_2P.fastq.gz %s_2U.fastq.gz ' \
-#                        'ILLUMINACLIP:illumina_multiplex.fa:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36' % (name1, name2, name1, name1, name2, name2)
-#     print(trimming_command)
-#     os.system(trimming_command)
-#
-#     # Fastqc
-#     fastqc_command = 'fastqc %s_1P.fastq.gz %s_2P.fastq.gz' % (name1, name2)
-#     multiqc_command = 'multiqc %s_1P.fastq.gz %s_2P.fastq.gz' % (name1, name2)
-#     print(fastqc_command)
-#     print(multiqc_command)
-#     os.system(fastqc_command)
-#     os.system(multiqc_command)
-#
-#     # STAR alignment
-#     STAR_command_reference = 'STAR --runMode genomeGenerate --runThreadN 30 --genomeDir TAIR10_STAR --sjdbGTFfile %s --genomeFastaFiles %s' % (options.annotation_gtf, options.genome)
-#     print(STAR_command_reference)
-#     os.system(STAR_command_reference)
-#     STAR_command_reads_sample1 = 'STAR --genomeDir TAIR10_STAR --runThreadN 30 --readFilesIn %s_1P.fastq.gz ' \
-#                          '--readFilesCommand zcat --quantMode GeneCounts --outFileNamePrefix %s.sorted.bam -' \
-#                          '-outSAMtype BAM SortedByCoordinate' % (samples[0]+replicate, samples[0]+replicate)
-#
-#     STAR_command_reads_sample2 = 'STAR --genomeDir TAIR10_STAR --runThreadN 30 --readFilesIn %s_2P.fastq.gz ' \
-#                          '--readFilesCommand zcat --quantMode GeneCounts --outFileNamePrefix %s.sorted.bam -' \
-#                          '-outSAMtype BAM SortedByCoordinate' % (samples[1] + replicate, samples[1] + replicate)
-#
-#     print(STAR_command_reads_sample1)
-#     print(STAR_command_reads_sample2)
-#     os.system(STAR_command_reads_sample1)
-#     os.system(STAR_command_reads_sample2)
-#
-#     # count reads and expression matrix based star BAM
-#     # Create a stringtie samples file
-#     stringtie_command_sample1 = 'stringtie %s.sorted.bamAligned.sortedByCoord.out.bam -p 30 -G %s -e -o %s.gtf -A %s.gene_abundances.tsv' % \
-#                         (samples[0]+replicate, options.annotation_gff, samples[0]+replicate, samples[0]+replicate,)
-#
-#     stringtie_command_sample2 = 'stringtie %s.sorted.bamAligned.sortedByCoord.out.bam -p 30 -G %s -e -o %s.gtf -A %s.gene_abundances.tsv' % \
-#                                 (samples[1] + replicate, options.annotation_gff, samples[1] + replicate,
-#                                  samples[1] + replicate,)
-#     print(stringtie_command_sample1)
-#     print(stringtie_command_sample2)
-#     os.system(stringtie_command_sample1)
-#     os.system(stringtie_command_sample2)
+for replicate in replicates:
+    name1 = options.path+'/'+samples[0]+replicate
+    name2 = options.path+'/'+samples[1]+replicate
+    print(name1, name2)
+    # Trimming the reads
+    trimming_command = 'trimmomatic PE -threads 30 -phred33 %s.fastq.gz' \
+                       ' %s.fastq.gz %s_1P.fastq.gz %s_1U.fastq.gz %s_2P.fastq.gz %s_2U.fastq.gz ' \
+                       'ILLUMINACLIP:illumina_multiplex.fa:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36' % (name1, name2, name1, name1, name2, name2)
+    print(trimming_command)
+    os.system(trimming_command)
+
+    # Fastqc
+    fastqc_command = 'fastqc %s_1P.fastq.gz %s_2P.fastq.gz' % (name1, name2)
+    multiqc_command = 'multiqc %s_1P.fastq.gz %s_2P.fastq.gz' % (name1, name2)
+    print(fastqc_command)
+    print(multiqc_command)
+    os.system(fastqc_command)
+    os.system(multiqc_command)
+
+    # STAR alignment
+    STAR_command_reference = 'STAR --runMode genomeGenerate --runThreadN 30 --genomeDir TAIR10_STAR --sjdbGTFfile %s --genomeFastaFiles %s' % (options.annotation_gtf, options.genome)
+    print(STAR_command_reference)
+    os.system(STAR_command_reference)
+    STAR_command_reads_sample1 = 'STAR --genomeDir TAIR10_STAR --runThreadN 30 --readFilesIn %s_1P.fastq.gz ' \
+                         '--readFilesCommand zcat --quantMode GeneCounts --outFileNamePrefix %s.sorted.bam -' \
+                         '-outSAMtype BAM SortedByCoordinate' % (samples[0]+replicate, samples[0]+replicate)
+
+    STAR_command_reads_sample2 = 'STAR --genomeDir TAIR10_STAR --runThreadN 30 --readFilesIn %s_2P.fastq.gz ' \
+                         '--readFilesCommand zcat --quantMode GeneCounts --outFileNamePrefix %s.sorted.bam -' \
+                         '-outSAMtype BAM SortedByCoordinate' % (samples[1] + replicate, samples[1] + replicate)
+
+    print(STAR_command_reads_sample1)
+    print(STAR_command_reads_sample2)
+    os.system(STAR_command_reads_sample1)
+    os.system(STAR_command_reads_sample2)
+
+    # count reads and expression matrix based star BAM
+    # Create a stringtie samples file
+    stringtie_command_sample1 = 'stringtie %s.sorted.bamAligned.sortedByCoord.out.bam -p 30 -G %s -e -o %s.gtf -A %s.gene_abundances.tsv' % \
+                        (samples[0]+replicate, options.annotation_gff, samples[0]+replicate, samples[0]+replicate,)
+
+    stringtie_command_sample2 = 'stringtie %s.sorted.bamAligned.sortedByCoord.out.bam -p 30 -G %s -e -o %s.gtf -A %s.gene_abundances.tsv' % \
+                                (samples[1] + replicate, options.annotation_gff, samples[1] + replicate,
+                                 samples[1] + replicate,)
+    print(stringtie_command_sample1)
+    print(stringtie_command_sample2)
+    os.system(stringtie_command_sample1)
+    os.system(stringtie_command_sample2)
 
 # Create the samples.txt file
 samples_txt = open('prepDE_samples.txt', 'w')
